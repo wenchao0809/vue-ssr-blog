6b2198679b4e3fee0682d3b40e1b3c8627ad84e8
BESTDing <estding0@gmail.com>
2017-10-07 19:29:30 +0800

    no

--- build/main.js
+++ build/main.js
@@ -83,15 +83,9 @@ module.exports = {
   jwtSecret: 'estding',
   db: {
     host: 'localhost',
-<<<<<<< HEAD
     user: 'estdingblog',
     // password: '123456',
     password: 'Blogding912*',
-=======
-    user: 'root',
-    password: '123456',
-    // password: 'ding912823',
->>>>>>> eb5e4c4f76dc289beae1e39cf952e64a9aeee33f
     database: 'estding'
   },
   needAuth: ['/api/classify/add',
