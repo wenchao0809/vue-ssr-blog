f24801f464ebbf5e4d05b908fb4fb2d40d1fbd52
BESTDing <estding0@gmail.com>
2017-10-16 18:08:37 +0800

    no message

--- util/http.js
+++ util/http.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
 
-if (process.SERVER_BUILD) {
-  axios.defaults.baseURL = 'http://estding.com:3000'
-}
+axios.defaults.baseURL = 'http://estding.com:3000'
 export default axios
